7e722e77019d377ea20d81ce256e427785ca5fda
Carter Dixon <115120759+carterjohndixon@users.noreply.github.com>
2026-01-25 21:07:30 -0800

    Delete migrations directory

--- migrations/0009_add_slack_workspace_id_to_integrations.sql
+++
@@ -1,6 +0,0 @@
--- Add slack_workspace_id column to integrations table
--- This links integrations to specific Slack workspaces for multi-workspace support
-ALTER TABLE "integrations" ADD COLUMN "slack_workspace_id" integer;
-
--- Create index for faster lookups
-CREATE INDEX IF NOT EXISTS "integrations_slack_workspace_id_idx" ON "integrations" ("slack_workspace_id");
