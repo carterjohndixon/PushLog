798d37ee9cf3297e203758caadd7d3d41f580b84
carterjohndixon <carter@m0nke.com>
2026-02-03 21:21:50 -0800

    fixing little issues

--- server/vite.ts
+++ server/vite.ts
@@ -73,6 +73,8 @@ export async function setupVite(app: Express, server: Server) {
 
 export function serveStatic(app: Express) {
   const distPath = path.resolve(__dirname, "public");
+  const indexInPublic = path.resolve(distPath, "index.html");
+  const indexInDist = path.resolve(__dirname, "index.html");
 
   if (!fs.existsSync(distPath)) {
     throw new Error(
@@ -82,8 +84,22 @@ export function serveStatic(app: Express) {
 
   app.use(express.static(distPath));
 
-  // fall through to index.html if the file doesn't exist
-  app.use("*", (_req, res) => {
-    res.sendFile(path.resolve(distPath, "index.html"));
+  // fall through to index.html if the file doesn't exist (SPA routes)
+  app.use("*", (req, res) => {
+    const indexPath = fs.existsSync(indexInPublic)
+      ? indexInPublic
+      : fs.existsSync(indexInDist)
+        ? indexInDist
+        : null;
+    if (indexPath) {
+      res.sendFile(indexPath);
+    } else {
+      console.error(
+        `ENOENT: no such file or directory, stat '${indexInPublic}' (also tried '${indexInDist}')`
+      );
+      res.status(404).send(
+        "Client build not found. Run: npm run build (vite build + server bundle)."
+      );
+    }
   });
 }
