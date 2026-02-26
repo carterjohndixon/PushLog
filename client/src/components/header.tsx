81a2f7d146a03dbc0b2a95463a3f5a53d4673739
carterjohndixon <carter@m0nke.com>
2026-02-25 18:44:15 -0800

    Maybe moving the header all the way to the left will fix this

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -71,7 +71,7 @@ export function Header() {
 
   return (
     <header className="bg-background border-b border-border sticky top-0 z-50">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="w-full px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex items-center space-x-4">
             <Link href="/" className="flex items-center space-x-3 flex-shrink-0">
